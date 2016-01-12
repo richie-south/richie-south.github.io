3b876bbd8972833472eebdf80375f17a3721f1ea
rs222kn <richard.soderman@gmail.com>
2016-01-12 16:05:05 +0100

    added scroll menu

--- script/main.js
+++ script/main.js
@@ -3,9 +3,31 @@
 var scr = scr || {};
 
 scr.init = function(){
+    console.log("init");
     //scr.menu();
     //scr.profileImage();
+    scr.mainMenu();
+};
 
+scr.mainMenu = function(callback){
+    var menuMove = 325;
+    var menu = document.querySelector("#menu");
+    var leftMenuButton = menu.querySelector(".leftMenuButton");
+    var rightMenuButton = menu.querySelector(".rightMenuButton");
+
+    window.addEventListener("scroll", function(e){
+
+        var scrollPos = window.pageYOffset;
+        if(scrollPos > menuMove){
+            menu.setAttribute("class", "menuFixed");
+            leftMenuButton.classList.remove("hide");
+            rightMenuButton.classList.remove("hide");
+        }else{
+            menu.removeAttribute("class", "menuFixed");
+            leftMenuButton.classList.add("hide");
+            rightMenuButton.classList.add("hide");
+        }
+    });
 };
 
 scr.profileImage = function(){
@@ -26,12 +48,12 @@ scr.menu = function(){
     imageDiv.style.visibility = 'hidden';
 
     window.addEventListener('scroll', function(e){
-        max = document.body.scrollHeight - innerHeight;
-        percent = (pageYOffset / max) * 100;
+        max = document.body.scrollHeight - window.innerHeight;
+        percent = (window.pageYOffset / max) * 100;
 
         imageDiv.style.height = percent*8 +'px';
         imageDiv.style.width = percent*8 +'px';
-        imageDiv.style.visibility = ''
+        imageDiv.style.visibility = '';
 
         if(percent === 0){
             imageDiv.style.height = '1px';
@@ -42,4 +64,4 @@ scr.menu = function(){
     });
 };
 
-window.onLoad = new scr.init;
+window.onload = scr.init;

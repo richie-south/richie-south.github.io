352fb842d0f2c5ede4f7abcaac396c049d0ffc0b
rs222kn <rs222kn@student.lnu.se>
2017-02-24 14:57:02 +0100

    added dot

--- script/main.js
+++ script/main.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const colors = ['#0ADFFC', '#FFDA04', '#BA52F7', '#11ED38'];
-
-
-
 
 const random = (nr) => {
   return Math.floor(Math.random() * nr);
@@ -13,8 +9,9 @@ const starSize = () => {
   return Math.random() * 2;
 };
 
-class Star {
-  constructor(x, y, size, start, stop, color, id){
+const Star = class {
+
+  constructor(x, y, size, start, stop){
     this.x = x;
     this.y = y;
     this.size = size;
@@ -22,9 +19,8 @@ class Star {
     this.stop = stop * Math.PI;
     this.width = 60 + size;
     this.height = 60 + size;
+
     this.visited = false;
-    this.color = color;
-    this.id = id;
   }
 
   colision(star){
@@ -38,28 +34,42 @@ class Star {
     let amount = 0;
 
     const drawInterval = setInterval(() => {
-      canvas.beginPath();
       if (amount > 1) {
-      	amount = 1;
         clearInterval(drawInterval);
         done();
-        //const audio = new Audio('./blop.mp3');
-        //audio.play();
-        //createCanvasStar(canvas, star.x, star.y, star.size, star.start, star.stop);
       }
+      canvas.beginPath();
       canvas.lineWidth = 0.2;
       canvas.strokeStyle = '#ff7280';
       canvas.moveTo(this.x, this.y);
-
       canvas.lineTo(this.x + (star.x - this.x) * amount, this.y + (star.y - this.y) * amount);
       canvas.stroke();
 
-      amount += 0.08;
 
-    }, Math.random() * 50);
+      amount += 0.06;
+    }, Math.random() * 100);
   }
-}
+};
 
+const shuffle = (a) => {
+    let j, x, i;
+    const array = a.slice();
+    for (i = array.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = array[i - 1];
+        array[i - 1] = array[j];
+        array[j] = x;
+    }
+  return array;
+};
+
+const connectStars = (canvas, stars, root) =>{
+  stars
+    .filter(star => !star.visited && root.colision(star))
+    .map(star => { star.visited = true; return star; })
+    .forEach(star =>
+      root.drawLineTo(canvas, star, connectStars.bind(null, canvas, stars, star)));
+};
 
 const createCanvasStar = (canvas, x, y, size, start, stop, color) => {
   canvas.fillStyle = color || '#FFFFFF';
@@ -69,56 +79,10 @@ const createCanvasStar = (canvas, x, y, size, start, stop, color) => {
   canvas.fill();
 };
 
-const connectStar = (canvas, stars, root) =>{
-  stars
-    .filter(star => !star.visited && root.colision(star))
-    .map(star => { star.visited = true; return star; })
-    .forEach(star =>
-      root.drawLineTo(canvas, star, connectStar.bind(null, canvas, stars, star)));
-};
-
-// const canvasClick = (canvasElement, canvas, stars) => {
-//   canvasElement.addEventListener('click', (e) => {
-//     console.log('hej');
-//     const rect = canvasElement.getBoundingClientRect();
-//     const pos = {
-//       x: e.clientX - rect.left,
-//       y: e.clientY - rect.top
-//     };
-//     const clickStar = new Star(pos.x, pos.y, 1, 0, 2, colors[Math.floor(Math.random()*colors.length)], 'mouse');
-//
-//     const inside = stars.filter(star => clickStar.colision(star));
-//
-//     connectStar(canvas, stars, inside[Math.floor(Math.random()*inside.length)]);
-//   });
-// };
-
-// const mosstConnected = (stars) => {
-//   let star;
-//   let max = 0;
-//   for (let i = 0; i < stars.length; i++) {
-//     let tmp = 0;
-//     for (let k = 0; k < stars.length; k++) {
-//       if(stars[i].colision(stars[k])){
-//         tmp++;
-//       }
-//     }
-//     if(tmp > max){
-//       max = tmp;
-//       star = stars[i];
-//
-//     }
-//   }
-//
-//   return star;
-// };
-
 const background = (canvasElement) => {
   canvasElement.height = window.innerHeight;
   canvasElement.width = window.innerWidth;
   const canvas = canvasElement.getContext('2d');
-
-
 
   const stars = [];
   for (let i = 0; i < (window.innerWidth / 4); i++) {
@@ -126,9 +90,7 @@ const background = (canvasElement) => {
       random(window.innerWidth),
       random( window.innerHeight),
       starSize(),
-      0, 2,
-      colors[Math.floor(Math.random()*colors.length)],
-      i
+      0, 2
     );
     stars.push(star);
 
@@ -140,16 +102,15 @@ const background = (canvasElement) => {
       star.start,
       star.stop);
   }
-
-  //connectStar(canvas, stars, mosstConnected(stars));
-  //canvasClick(canvasElement, canvas, stars);
-  connectStar(canvas, stars, stars[Math.floor(Math.random()*stars.length)]);
+  return [canvas, stars];
 };
 
-const init = function(){
+const init = () => {
   const canvasElement = document.querySelector('#canvas');
 
-  background(canvasElement);
+  const [canvas, stars] = background(canvasElement);
+  connectStars(canvas, stars, stars[Math.floor(Math.random()*stars.length)]);
+
   window.addEventListener('resize', function(event){
     background(canvasElement);
   });
